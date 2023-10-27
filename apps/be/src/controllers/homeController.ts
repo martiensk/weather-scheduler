70e5ea3ed19e8bb2443c70e1405e57b7c490c664
Martiens Kropff <martiens@annexio.com>
2023-10-27 14:32:06 +0100

    Added DB + Caching

--- apps/be/src/controllers/homeController.ts
+++ apps/be/src/controllers/homeController.ts
@@ -1,5 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
+/**
+ * Handles the home route.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next function.
+ */
 export const home = (req: Request, res: Response, next: NextFunction) => {
     res.end('OK')
 }
